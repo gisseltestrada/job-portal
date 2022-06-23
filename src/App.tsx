e06b52eb080fa291a300caa7cdf01ba425bf71b6
ServandoL <swyswy214@gmail.com>
2022-06-22 19:25:30 -0500

    convert typescript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { SignIn } from '../src/components/sign-in';
+
+function App() {
+  return (
+    <div className="App">
+      <SignIn />
+    </div>
+  );
+}
+
+export default App;
